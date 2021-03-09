817088511708709d07ee7cba88e01010f43ff326
ludvig <ludviglinde@outlook.com>
2021-03-09 12:54:44 +0100

    post

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+ @app.route("/login",methods=["POST"])
+ def login():
+     return render_template("login.html")
 
 app.run(host="0.0.0.0")
