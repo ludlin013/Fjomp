0a95771f0c340d60d51e40e9ccf06872368e26ed
ludvig <ludviglinde@outlook.com>
2021-03-09 13:26:28 +0100

    form start

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
+
+def checklogin(username,password):
+    print(username)
+    print(password)
+
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
- @app.route("/login",methods=["POST"])
- def login():
-     return render_template("login.html")
+@app.route("/login",methods=["GET","POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if checklogin(request.form['username'],
+            request.form['password']):
+            return main()
+        else:
+            error = "Invalid"
+    return render_template("login.html")
 
 app.run(host="0.0.0.0")
