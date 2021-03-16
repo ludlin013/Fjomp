9f349198e235042856b873bd98a312457110b1d3
ludvig <ludviglinde@outlook.com>
2021-03-16 12:44:32 +0100

    settings byta lösenord

--- main.py
+++ main.py
@@ -31,7 +31,12 @@ def sql(type,sqlquery):
 def checklogin(username,password):
     usrs = sql("SELECT","SELECT Tech_ID, Tech_Firstname, Tech_Lastname, Tech_Pwd FROM Technicians")
     for usr in usrs:
-        if username == usr[0].strip() and password == usr[3].strip():
+        if usr[0].strip() == username and usr[3] == None:
+            #print("set " + usr[0] + " password to " + password)
+            #print("UPDATE Technicians SET Tech_Pwd = '" + password + "' WHERE Tech_ID = '"+ username +"'")
+            sql("INSERT","UPDATE Technicians SET Tech_Pwd = '" + password + "' WHERE Tech_ID = '"+ usr[0] +"'")
+
+        elif username == usr[0].strip() and password == usr[3].strip():
             return True
     return False
 
@@ -272,6 +277,7 @@ def settings():
     theme,notheme = setTheme()
 
     techs = sql("SELECT","SELECT * FROM Technicians")
+    vendors = sql("SELECT","SELECT * FROM Vendors")
 
     #print(techs)
 
@@ -282,24 +288,66 @@ def settings():
         techoffice = request.form.getlist('office')
         techtech = request.form.getlist('tech')
 
+        sqltechs = []
+        updtechs = []
 
-####
-##
-#
-        #göras om här
-        for i in range(len(techid)):
-            for x in techs:
-                if techid[i] == x[0]:
-                    if techfirst[i] == x[1].strip() and techlast[i] == x[2].strip():
-                        break
-            if techid[i] == "" or techfirst[i] == "" or techlast[i] == "" or techoffice[i] == "" or techtech[i] == "":
-                break
+        for x in techs:
+            sqltechs.append((x[0],x[1].strip(),x[2].strip(),str(x[4]),str(x[5])))
+
+        print("=====================")
+        for x in range(len(techid)):
+            updtechs.append((techid[x],techfirst[x],techlast[x],techoffice[x],techtech[x]))
+
+        deleteuser = [list(set(sqltechs) - set(updtechs))]
+        newuser = [list(set(updtechs) - set(sqltechs))]
+
+        for x in newuser[0]:
+            if x[0] == "" or x[1] == "" or x[2] == "" or x[3] == "" or x[4] == "":
+                pass
             else:
-                sql("INSERT","INSERT INTO Technicians (Tech_ID,Tech_Firstname,Tech_Lastname,Tech_Office,Tech_Tech) VALUES ('" + techid[i].upper()+"','"+ techfirst[i]+"','"+ techlast[i]+"','"+ techoffice[i]+"','"+ techtech[i]+"')")
+                sql("INSERT","INSERT INTO Technicians (Tech_ID,Tech_Firstname,Tech_Lastname,Tech_Office,Tech_Tech) VALUES ('" + x[0].upper()+"','"+ x[1]+"','"+ x[2]+"','"+ x[3]+"','"+ x[4]+"')")
+
+        for x in deleteuser[0]:
+            #print("DELETE FROM Technicians WHERE Tech_ID = '" + x[0] +"'")
+            sql("INSERT","DELETE FROM Technicians WHERE Tech_ID = '" + x[0] +"'")
+
         return redirect(url_for("settings"))
 
 
-    return render_template("settings.html",theme=theme,notheme=notheme,auth=authenticated,techs=techs)
+    return render_template("settings.html",theme=theme,notheme=notheme,auth=authenticated,techs=techs,vendors=vendors)
+
+@app.route("/settings/changepassword",methods=["GET","POST"])
+def changepwd():
+    if "loggedin" in request.cookies:
+        pass
+    else:
+        return redirect(url_for("login"))
+    theme,notheme = setTheme()
+    if "loggedin" in request.cookies:
+        usr = request.cookies.get('username')
+    error = None
+    print(usr)
+    oldpass = sql("SELECT","SELECT Tech_Pwd FROM Technicians WHERE Tech_ID = '" + usr +"'")[0][0].strip()
+
+    print(oldpass)
+
+    if request.method == 'POST':
+        oldpwd = request.form["oldpwd"]
+        newpwd1 = request.form["newpwd"]
+        newpwd2 = request.form["newpwd2"]
+
+        print([oldpwd,newpwd1,newpwd2])
+        if oldpwd == oldpass:
+            if newpwd1 == newpwd2 and newpwd1 != "" and newpwd2 != "":
+                sql("INSERT","UPDATE Technicians SET Tech_Pwd = '" + newpwd1 + "' WHERE Tech_ID = '"+ usr +"'")
+                return render_template("passwordchanged.html",theme=theme,notheme=notheme)
+            else:
+                error = "Password don't match"
+        else:
+            error = "Invalid password"
+
+
+    return render_template("changepwd.html",theme=theme,notheme=notheme,error=error)
 
 
 app.run(host="0.0.0.0",port="80")
