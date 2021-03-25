4901176ae94e6b2adfa78ff38135225ce9098bdf
ludvig <ludviglinde@outlook.com>
2021-03-25 14:41:36 +0100

    mail o litta

--- static/irjs.js
+++ static/irjs.js
@@ -23,3 +23,23 @@ function changerepact(idd){
   }
   document.getElementById(idd).style.display = "block"
 }
+
+var active = false;
+
+function setday(id){
+  var y = new Date().getFullYear();
+  var m = "0" + (new Date().getMonth()+1);
+  var d = "0" + new Date().getDate();
+
+  if (m.length>2){
+    m = m.substring(1)
+  }
+
+  if (d.length>2){
+    d = d.substring(1)
+  }
+
+  var date = y + "-"+m+"-"+d
+  id.value = date;
+  window.getSelection().removeAllRanges()
+}
