d0aab9ddc37822c57717f135b6ce1f68768f7a46
Bobbimo <31735133+Bobbimo@users.noreply.github.com>
2021-03-25 15:50:44 +0100

    Sökningen funkar nu! Alternativ vid halva ordet ska finnas...

--- lookup.py
+++ lookup.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 from __main__ import *
 import pyodbc
 import datetime
+from datetime import datetime
 
 server = "10.3.1.193,50404\\FJOMP"
 database = "Winstat"
@@ -48,6 +49,14 @@ def lookup():
     partname = ""
     looknumname = ""
     lookserial = ""
+    current_date = datetime.now().date()
+    rbball = "exact"
+
+    btwdate1 = datetime(2010, 1, 1).date()
+    btwdate2 = current_date
+
+    print([btwdate1])
+    print([btwdate2])
     lookupdata = []
     delivnote_result = []
     irparts_result = []
@@ -61,6 +70,9 @@ def lookup():
         looknumname = request.form["lookupnumname"]
         lookserial = request.form["lookupserial"]
         rbball = request.form["radiobuttons-lookup"]
+        btwdate1 = datetime.strptime(request.form["btwdate1"], '%Y-%m-%d').date()
+        btwdate2 = datetime.strptime(request.form["btwdate2"], '%Y-%m-%d').date()
+
 
 
         try:
@@ -108,10 +120,10 @@ def lookup():
 
 
         if looknumname != "":
-            delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE DN_Part ='"+looknumname+"' OR DN_Partno = '"+looknumname+"'")
-            irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE IRP_Part ='"+looknumname+"' OR IRP_Partno = '"+looknumname+"'")
-            sentswap = sql("SELECT", "SELECT * FROM Swap WHERE SWP_NewPart ='"+looknumname+"' OR SWP_NewPartno = '"+looknumname+"'")
-            returnedswap = sql("SELECT", "SELECT * FROM Swap WHERE SWP_OldPart ='"+looknumname+"' OR SWP_OldPartno = '"+looknumname+"'")
+            delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE UPPER(DN_Part) ='"+looknumname.upper()+"' OR UPPER(DN_Partno) = '"+looknumname.upper()+"'")
+            irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE UPPER(IRP_Part) ='"+looknumname.upper()+"' OR UPPER(IRP_Partno) = '"+looknumname.upper()+"'")
+            sentswap = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_NewPart) ='"+looknumname.upper()+"' OR UPPER(SWP_NewPartno) = '"+looknumname.upper()+"'")
+            returnedswap = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_OldPart) ='"+looknumname.upper()+"' OR UPPER(SWP_OldPartno) = '"+looknumname.upper()+"'")
 
 
 
@@ -119,179 +131,184 @@ def lookup():
 
             if rbball == "exact":
 
-                delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE DN_Serial = '"+lookserial+"'")
-                irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE IRP_Serial = '"+lookserial+"'")
-                sentswap_results = sql("SELECT", "SELECT * FROM Swap WHERE SWP_NewSerial = '"+lookserial+"'")
-                returnedswap_results = sql("SELECT", "SELECT * FROM Swap WHERE SWP_OldSerial ='"+lookserial+"'")
-                unitsfile_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '0' AND Unit_Serial = '"+lookserial+"'")
-                unitshistory_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '1' AND Unit_Serial = '"+lookserial+"'")
+                delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE UPPER(DN_Serial) = '"+lookserial.upper()+"'")
+                irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE UPPER(IRP_Serial) = '"+lookserial.upper()+"'")
+                sentswap_results = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_NewSerial) = '"+lookserial.upper()+"'")
+                returnedswap_results = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_OldSerial) ='"+lookserial.upper()+"'")
+                unitsfile_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '0' AND UPPER(Unit_Serial) = '"+lookserial.upper()+"'")
+                unitshistory_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '1' AND UPPER(Unit_Serial) = '"+lookserial.upper()+"'")
 
-                print("IF lookserial")
-                print(unitsfile_results, unitshistory_results)
 
             elif rbball == "partoff":
 
-                delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE DN_Serial LIKE'%"+lookserial+"%'")
-                irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE IRP_Serial LIKE '%"+lookserial+"%'")
-                sentswap_results = sql("SELECT", "SELECT * FROM Swap WHERE SWP_NewSerial LIKE '%"+lookserial+"%'")
-                returnedswap_results = sql("SELECT", "SELECT * FROM Swap WHERE SWP_OldSerial LIKE '%"+lookserial+"%'")
-                unitsfile_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '0' AND Unit_Serial LIKE '%"+lookserial+"%'")
-                unitshistory_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '1' AND Unit_Serial LIKE '%"+lookserial+"%'")
+                delivnote_result = sql("SELECT", "SELECT * FROM DelivNotes WHERE UPPER(DN_Serial) LIKE'%"+lookserial.upper()+"%'")
+                irparts_result = sql("SELECT", "SELECT * FROM IRParts WHERE UPPER(IRP_Serial) LIKE '%"+lookserial.upper()+"%'")
+                sentswap_results = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_NewSerial) LIKE '%"+lookserial.upper()+"%'")
+                returnedswap_results = sql("SELECT", "SELECT * FROM Swap WHERE UPPER(SWP_OldSerial) LIKE '%"+lookserial.upper()+"%'")
+                unitsfile_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '0' AND UPPER(Unit_Serial) LIKE '%"+lookserial.upper()+"%'")
+                unitshistory_results = sql("SELECT", "SELECT * FROM Units WHERE Unit_History = '1' AND UPPER(Unit_Serial) LIKE '%"+lookserial.upper()+"%'")
 
 
 
         if checked_delivnotes == "1":
             for x in delivnote_result:
-                Dict = {}
-                Dict["url"] = "/delivnotes?dn="+str(x[1])
-                Dict["type"] = "Deliverynote"
-                Dict["ref"] = x[1]
-                Dict["customerID"] = x[0]
-                Dict["serial"] = x[7]
-                Dict["date"] = x[4]
+                if btwdate1 < x[4].date() < btwdate2:
+                    Dict = {}
+                    Dict["url"] = "/delivnotes?dn="+str(x[1])
+                    Dict["type"] = "Deliverynote"
+                    Dict["ref"] = x[1]
+                    Dict["customerID"] = x[0]
+                    Dict["serial"] = x[7]
+                    Dict["date"] = x[4]
 
-                try:
-                    Dict["customerName"] = cust[x[0].strip()]
+                    try:
+                        Dict["customerName"] = cust[x[0].strip()]
 
-                except Exception:
-                    Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                    except Exception:
+                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                if x[4] != None:
-                    Dict["date_check"] = x[4].date()
+                    if x[4] != None:
+                        Dict["date_check"] = x[4].date()
 
-                else:
-                    Dict["date_check"] = datetime.date(1900,1,1)
+                    else:
+                        Dict["date_check"] = datetime.date(1900,1,1)
 
-                lookupdata.append(Dict)
+                    lookupdata.append(Dict)
 
 
         if checked_irparts == "1":
             for x in irparts_result:
-                Dict = {}
-                Dict["url"] = "/ir?ir="+str(x[1])
-                Dict["type"] = "IR Parts Used"
-                Dict["ref"] = x[1]
-                Dict["customerID"] = x[0]
-                Dict["serial"] = x[3]
-                Dict["date"] = x[10]
+                if btwdate1 < x[10].date() < btwdate2:
+                    Dict = {}
+                    Dict["url"] = "/ir?ir="+str(x[1])
+                    Dict["type"] = "IR Parts Used"
+                    Dict["ref"] = x[1]
+                    Dict["customerID"] = x[0]
+                    Dict["serial"] = x[3]
+                    Dict["date"] = x[10]
 
-                try:
-                    Dict["customerName"] = cust[x[0].strip()]
+                    try:
+                        Dict["customerName"] = cust[x[0].strip()]
 
-                except Exception:
-                    Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                    except Exception:
+                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                if x[10] != None:
-                    Dict["date_check"] = x[10].date()
+                    if x[10] != None:
+                        Dict["date_check"] = x[10].date()
 
-                else:
-                    Dict["date_check"] = datetime.date(1900,1,1)
+                    else:
+                        Dict["date_check"] = datetime.date(1900,1,1)
 
-                lookupdata.append(Dict)
+                    lookupdata.append(Dict)
 
 
         if checked_sent_swapout == "1":
             for x in sentswap_results:
-                Dict = {}
-                Dict["url"] = "/swapouts?so="+str(x[1])
-                Dict["type"] = "Sent-Swapouts"
-                Dict["ref"] = x[1]
-                Dict["customerID"] = x[0]
-                Dict["date"] = x[3]
-                Dict["serial"] = x[9]
+                if btwdate1 < x[3] < btwdate2:
+                    Dict = {}
+                    Dict["url"] = "/swapouts?so="+str(x[1])
+                    Dict["type"] = "Sent-Swapouts"
+                    Dict["ref"] = x[1]
+                    Dict["customerID"] = x[0]
+                    Dict["date"] = x[3]
+                    Dict["serial"] = x[9]
 
-                try:
-                    Dict["customerName"] = cust[x[0].strip()]
+                    try:
+                        Dict["customerName"] = cust[x[0].strip()]
 
-                except Exception:
-                    Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                    except Exception:
+                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                if x[3] != None:
-                    Dict["date_check"] = x[3]
+                    if x[3] != None:
+                        Dict["date_check"] = x[3]
 
-                else:
-                    Dict["date_check"] = datetime.date(1900,1,1)
+                    else:
+                        Dict["date_check"] = datetime.date(1900,1,1)
 
-                lookupdata.append(Dict)
+                    lookupdata.append(Dict)
 
 
         if checked_returned_swapout == "1":
             for x in returnedswap_results:
-                Dict = {}
-                Dict["url"] = "/swapouts?so="+str(x[1])
-                Dict["type"] = "Returned-Swapouts"
-                Dict["ref"] = x[1]
-                Dict["customerID"] = x[0]
-                Dict["date"] = x[3]
-                Dict["serial"] = x[6]
+                if btwdate1 < x[3] < btwdate2:
+                    Dict = {}
+                    Dict["url"] = "/swapouts?so="+str(x[1])
+                    Dict["type"] = "Returned-Swapouts"
+                    Dict["ref"] = x[1]
+                    Dict["customerID"] = x[0]
+                    Dict["date"] = x[3]
+                    Dict["serial"] = x[6]
 
-                try:
-                    Dict["customerName"] = cust[x[0].strip()]
+                    try:
+                        Dict["customerName"] = cust[x[0].strip()]
 
-                except Exception:
-                    Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                    except Exception:
+                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                if x[3] != None:
-                    Dict["date_check"] = x[3]
+                    if x[3] != None:
+                        Dict["date_check"] = x[3]
 
-                else:
-                    Dict["date_check"] = datetime.date(1900,1,1)
+                    else:
+                        Dict["date_check"] = datetime.date(1900,1,1)
 
-                lookupdata.append(Dict)
+                    lookupdata.append(Dict)
 
 
         if checked_unit_file == "1":
             if unitsfile_results != None:
                 for x in unitsfile_results:
-                    Dict = {}
-                    Dict["url"] = "/"
-                    Dict["type"] = "Unit-File"
-                    Dict["ref"] = ""
-                    Dict["customerID"] = x[0]
-                    Dict["date"] = x[6]
-                    Dict["serial"] = x[4]
+                    if btwdate1 < x[6] < btwdate2:
+                        Dict = {}
+                        Dict["url"] = "/"
+                        Dict["type"] = "Unit-File"
+                        Dict["ref"] = ""
+                        Dict["customerID"] = x[0]
+                        Dict["date"] = x[6]
+                        Dict["serial"] = x[4]
 
-                    try:
-                        Dict["customerName"] = cust[x[0].strip()]
+                        try:
+                            Dict["customerName"] = cust[x[0].strip()]
 
-                    except Exception:
-                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                        except Exception:
+                            Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                    if x[6] != None:
-                        Dict["date_check"] = x[6]
+                        if x[6] != None:
+                            Dict["date_check"] = x[6]
 
-                    else:
-                        Dict["date_check"] = datetime.date(1900,1,1)
+                        else:
+                            Dict["date_check"] = datetime.date(1900,1,1)
 
-                    lookupdata.append(Dict)
+                        lookupdata.append(Dict)
 
 
         if checked_unit_history == "1":
             if unitshistory_results != None:
                 for x in unitshistory_results:
-                    Dict = {}
-                    Dict["url"] = "/"
-                    Dict["type"] = "Unit-History"
-                    Dict["ref"] = ""
-                    Dict["customerID"] = x[0]
-                    Dict["date"] = x[6]
-                    Dict["serial"] = x[4]
+                    if btwdate1 < x[6] < btwdate2:
+                        Dict = {}
+                        Dict["url"] = "/"
+                        Dict["type"] = "Unit-History"
+                        Dict["ref"] = ""
+                        Dict["customerID"] = x[0]
+                        Dict["date"] = x[6]
+                        Dict["serial"] = x[4]
 
-                    try:
-                        Dict["customerName"] = cust[x[0].strip()]
+                        try:
+                            Dict["customerName"] = cust[x[0].strip()]
 
-                    except Exception:
-                        Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
+                        except Exception:
+                            Dict["customerName"] = sql("SELECT", "SELECT Cust_Name FROM Customers WHERE Cust_CustID = '" + x[0] + "'")
 
-                    if x[6] != None:
-                        Dict["date_check"] = x[6]
-                    else:
-                        Dict["date_check"] = datetime.date(1900,1,1)
+                        if x[6] != None:
+                            Dict["date_check"] = x[6]
+                        else:
+                            Dict["date_check"] = datetime.date(1900,1,1)
 
-                    lookupdata.append(Dict)
+                        lookupdata.append(Dict)
+
 
 
         lookupdata.sort(key = lambda x:x["date_check"], reverse=True)
 
 
-    return render_template("lookup.html",theme=theme,notheme=notheme,lookupdata=lookupdata, looknumname=looknumname, partname=partname)
+    return render_template("lookup.html",theme=theme,notheme=notheme,lookupdata=lookupdata, looknumname=looknumname, partname=partname, btwdate1=btwdate1, btwdate2=btwdate2, lookserial=lookserial, rbball=rbball)
